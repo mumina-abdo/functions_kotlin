d1cb40417c1b9fc8460821d826b5977a188572a7
mumina-abdo <muminaboruabdo@gmail.com>
2024-02-21 21:35:13 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main(){
+    myFunction( "Ada")
+    divide(7, 3)
+    divide(9, 2)
+    add(100, 68)
+    add(87, 45)
+    add(45, 56)
+    add(90, 35)
+    println("I love watching football")
+
+}
+
+fun myFunction(Hello: String){
+    println("Hello"+" " +"Ada")
+
+}
+
+fun divide(x:Int, y:Int){
+    var quotient = x%y
+    println(quotient)
+
+}
+fun add(x:Int, y:Int){
+    var sum = x+y
+    println(sum)
+
+}
+
